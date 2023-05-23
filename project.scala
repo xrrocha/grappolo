66100d32cd3d58ada01908315725ba81ed4d06ce
Ricardo Rocha <ricardo@xrrocha.net>
2023-05-22 20:59:16 -0500

    Initial commit

---
+++ project.scala
@@ -0,0 +1,12 @@
+//> using javaOpt -Xmx4g
+//> using scala 3.3.0-RC6
+//> using options -deprecation -release:11
+//> using resourceDirs src/main/resources src/test/resources
+
+// //> using options "-coverage-out:${.}"
+
+//> using jar lib/demetrio.jar
+//> using dep info.debatty:java-string-similarity:2.0.0
+//> using dep org.scala-lang.modules::scala-parallel-collections::1.0.4
+
+//> using test.dep org.scalameta::munit::1.0.0-M7
